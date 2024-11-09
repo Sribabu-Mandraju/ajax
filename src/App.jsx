6ea18d3de40232402f6dc0769ec046b296249b1f
Sireesha <n210413@rguktn.ac.in>
2024-11-09 02:58:03 -0800

    hi

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';  
+import './App.css';
+function App() {
+  return (
+    <div>
+      <div className="text-red-500 text-2xl">hi</div>
+      <div className="bg-black text-white p-4">hello</div>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';  // Import your Tailwind styles
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
