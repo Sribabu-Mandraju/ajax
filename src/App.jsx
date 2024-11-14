ff9de9097a35c3cdf0209e5bb9aeee61218a5260
sribabu <sribabumandraju@gmail.com>
2024-11-14 10:03:30 +0530

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 const App = () => {
   return (
     <div>
-      <div className="text-center w-full text-white bg-black">Hello</div>
+      <div className="text-center w-full text-white text-3xl bg-black">Hello</div>
     </div>
   )
 }
